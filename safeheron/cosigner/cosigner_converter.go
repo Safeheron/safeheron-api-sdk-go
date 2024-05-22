0bedd894822cb13c46c156e2856632b2aeb31b7a
absorprofess <absorprofess@outlook.com>
2024-05-22 14:25:14 +0800

    1.RequestTimeout,Default: 20000 2.Deprecate responseConverter 3.Add responseConverterWithNewCryptoType

--- safeheron/cosigner/cosigner_converter.go
+++ safeheron/cosigner/cosigner_converter.go
@@ -67,6 +67,7 @@ type CoSignerResponse struct {
 	TxKey   string `json:"txKey"`
 }
 
+// It has been Deprecated,Please use convertCoSignerResponseWithNewCryptoType
 func (c *CoSignerConverter) ResponseConverter(d any) (map[string]string, error) {
 	// Use AES to encrypt request data
 	aesKey := make([]byte, 32)
@@ -102,6 +103,46 @@ func (c *CoSignerConverter) ResponseConverter(d any) (map[string]string, error) 
 		return nil, err
 	}
 	params["sig"] = signature
+	return params, nil
+}
+
+func (c *CoSignerConverter) ResponseConverterWithNewCryptoType(d any) (map[string]string, error) {
+	// Use AES to encrypt request data
+	aesKey := make([]byte, 32)
+	rand.Read(aesKey)
+	aesIv := make([]byte, 16)
+	rand.Read(aesIv)
+	// Create params map
+	params := map[string]string{
+		"timestamp": strconv.FormatInt(time.Now().UnixMilli(), 10),
+		"code":      "200",
+		"message":   "SUCCESS",
+	}
+	if d != nil {
+		payLoad, _ := json.Marshal(d)
+		data := string(payLoad)
+		encryptBizContent, err := utils.EncryContentWithAESGCM(data, aesKey, aesIv)
+		if err != nil {
+			return nil, err
+		}
+		params["bizContent"] = encryptBizContent
+	}
+
+	// Use Safeheron RSA public key to encrypt request's aesKey and aesIv
+	encryptedKeyAndIv, err := utils.EncryptWithOAEP(append(aesKey, aesIv...), c.Config.ApiPubKey)
+	if err != nil {
+		return nil, err
+	}
+	params["key"] = encryptedKeyAndIv
+
+	// Sign the request data with your RSA private key
+	signature, err := utils.SignParamsWithRSA(serializeParams(params), c.Config.BizPrivKey)
+	if err != nil {
+		return nil, err
+	}
+	params["sig"] = signature
+	params["rsaType"] = utils.ECB_OAEP
+	params["aesType"] = utils.GCM
 	return params, nil
 }
 
